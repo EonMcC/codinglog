32bc10b91268f8586cb846ad92920754c32e22fe
Iain <iain2eyes@gmail.com>
2019-10-17 12:08:20 +0100

    remove global variables

--- CodingLogMac.rb
+++ CodingLogMac.rb
@@ -1,12 +1,12 @@
 
-$endprogram = 0
 def run_program()
-    until $endprogram == 1 do
+  $endprogram = 0
+  until $endprogram == 1 do
 
-puts "What would you like to do? \n\n    1. Start\n    2. Stop\n    3. Show Times\n    4. Manual Input\n    5. Exit"
-$choice = gets.chomp.downcase
+  puts "What would you like to do? \n\n    1. Start\n    2. Stop\n    3. Show Times\n    4. Manual Input\n    5. Exit"
+  choice = gets.chomp.downcase
 
-def which_action(choice)
+  def which_action(choice)
     case choice
     when "start", "1"
         return start()
@@ -22,60 +22,60 @@ def which_action(choice)
         puts "Not a valid option."
         run_program()
     end
-end
+  end
 #start starts the timer going.
-def start()
+  def start()
     puts "Timer Started.\n "
     $starting = Time.now
-end
+  end
 #stop will stop the timer then ask user for language before printing to the relevant .txt file.
-def stop()
+  def stop()
     puts "Timer Stopped.\n "
-    $ending = Time.now
-    $elapsed_time = $ending - $starting
-    $elapsed_time_minutes = $elapsed_time.to_i / 60
-    puts "Time spent programming: #{$elapsed_time_minutes}.\n "
+    ending = Time.now
+    elapsed_time = ending - $starting
+    elapsed_time_minutes = elapsed_time.to_i / 60
+    puts "Time spent programming: #{elapsed_time_minutes}.\n "
 
     puts "Which language were you coding with?\n "
-    $which_language = gets.chomp.downcase
-    case $which_language
+    which_language = gets.chomp.downcase
+    case which_language
     when "ruby"
         whole_file = File.open('ruby.txt').to_a
         last_time = whole_file.last()
-        $cumulative_time = last_time.to_f + $elapsed_time
+        cumulative_time = last_time.to_f + elapsed_time
         File.open('ruby.txt', 'a') do |file|
-            file.write("\n" + $cumulative_time.to_s) end
+            file.write("\n" + cumulative_time.to_s) end
     when "html"
         whole_file = File.open('html.txt').to_a
         last_time = whole_file.last()
-        $cumulative_time = last_time.to_f + $elapsed_time
+        cumulative_time = last_time.to_f + elapsed_time
         File.open('html.txt', 'a') do |file|
-            file.write("\n" + $cumulative_time.to_s) end
+            file.write("\n" + cumulative_time.to_s) end
     when "css"
         whole_file = File.open('css.txt').to_a
         last_time = whole_file.last()
-        $cumulative_time = last_time.to_f + $elapsed_time
+        cumulative_time = last_time.to_f + elapsed_time
         File.open('css.txt', 'a') do |file|
-            file.write("\n" + $cumulative_time.to_s) end
+            file.write("\n" + cumulative_time.to_s) end
     when "javascript", "js"
         whole_file = File.open('javascript.txt').to_a
         last_time = whole_file.last()
-        $cumulative_time = last_time.to_f + $elapsed_time
+        cumulative_time = last_time.to_f + elapsed_time
         File.open('javascript.txt', 'a') do |file|
-            file.write("\n" + $cumulative_time.to_s) end
+            file.write("\n" + cumulative_time.to_s) end
     when "test"
         whole_file = File.open('test.txt').to_a
         last_time = whole_file.last()
-        $cumulative_time = last_time.to_f + $elapsed_time
+        cumulative_time = last_time.to_f + elapsed_time
         File.open('test.txt', 'a') do |file|
-            file.write("\n" + $cumulative_time.to_s) end
+            file.write("\n" + cumulative_time.to_s) end
         else
             puts "\n*****Not a recognised language, try again.*****\n"
             stop()
     end
-end
+  end
 
-def times()
+  def times()
     ruby_whole_file = File.open('ruby.txt').to_a
     ruby_last_time = ruby_whole_file.last()
     ruby_minutes = ruby_last_time.to_f / 60
@@ -97,9 +97,9 @@ def times()
     HTML:       #{html_hours.to_i} hours | #{html_minutes.to_i} minutes | #{html_last_time.to_i} seconds \n
     CSS:        #{css_hours.to_i} hours | #{css_minutes.to_i} minutes | #{css_last_time.to_i} seconds \n
     JavaScript: #{javascript_hours.to_i} hours | #{javascript_minutes.to_i} minutes | #{javascript_last_time.to_i} seconds\n--------------------"
-end
+  end
 
-def manual()
+  def manual()
     puts "Which language would you like to enter a time for?"
     manual_input = gets.chomp.downcase
     case manual_input
@@ -147,11 +147,11 @@ def manual()
               last_time = whole_file.last()
               test_time = last_time.to_f + how_long_test_sec
               File.open('test.txt', 'a') do |file|
-                  file.write("\n" + test_time.to_s) end            
+                  file.write("\n" + test_time.to_s) end
     end
-end
-which_action($choice)
-end
+    end
+  which_action(choice)
+  end
 
 end
 
